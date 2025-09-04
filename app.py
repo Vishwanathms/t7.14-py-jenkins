f852566f0fbd456af1a7a04d0fb3ef67a53c3ea6
Vishwanathms <vishwanath.murthy@gmail.com>
2025-09-04 13:40:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Jenkins-Docker Pipeline!"
+    return "Hello from Jenkins-Docker Pipeline updated ! "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
